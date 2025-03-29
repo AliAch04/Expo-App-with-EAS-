eba6898d54b13ebe23124304d9d9cf795ebd0491
AliAch04 <ali.jallousy12@gmail.com>
2025-03-29 22:49:51 +0000

    adding DatePicker functionality

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,10 @@
+import React, {useState} from "react";
+import DatePicker from '@react-native-community/datetimepicker';
 import { Text, View } from "react-native";
 
 export default function Index() {
+  const [date, setDate] = useState(new Date());
+
   return (
     <View
       style={{
@@ -10,6 +14,14 @@ export default function Index() {
       }}
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
+      <DatePicker 
+        value={date}
+        onChange={(event, selectedDate) => {
+          const currentDate = selectedDate || date;
+          setDate(currentDate);
+        }}
+        mode="date" 
+      />
     </View>
   );
 }
